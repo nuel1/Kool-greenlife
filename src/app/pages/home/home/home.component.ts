070f59625ca33676eea30b6bda01e1555e1ea9ec
joseph <nwekejoseph52@gmail.com>
2024-02-03 10:59:23 -0800

    updates

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { withNoDomReuse } from '@angular/platform-browser';
+import {
+  createWidget,
+  createPopup,
+  Popup,
+  createSlider,
+} from '@typeform/embed';
+import * as tf from '@typeform/embed';
+// import AOS from 'aos';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  // @ViewChild('typeformContainer') typeFormEl: ElementRef | null = null;
+  // @ViewChild('typeformContainer') typeformContainer: ElementRef | null = null;
+  window: any = window;
+  ngOnInit() {
+    // Load the Typeform script dynamically
+    this.loadTypeformScript();
+  }
 
+  loadTypeformScript() {
+    const script = document.createElement('script');
+    script.src = 'https://embed.typeform.com/next/embed.js';
+    script.async = true;
+    script.onload = () => {
+      // Typeform script has loaded
+    };
+
+    document.body.appendChild(script);
+  }
 }
